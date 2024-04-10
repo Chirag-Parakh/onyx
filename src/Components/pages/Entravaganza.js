c02181458b41a9867feeae5a5970c2827cfe1631
Chirag Parakh <cparakh53@gmail.com>
2024-04-10 10:30:13 +0530

    origin

--- src/Components/pages/Entravaganza.js
+++ src/Components/pages/Entravaganza.js
@@ -2,29 +2,46 @@ import React from 'react'
 import ParticleBg from '../ParticleBg'
 import '../Entravaganz.css'
 
+
 function Entravaganza() {
-  return (
-    <div className='Entravaganza_Main'>
-        <div className='ParticleBg'>
-        <ParticleBg id="particles" className="particles"/>
-        </div>
-        <div className='Entravaganza_Heading'>
-        Entravaganza
-        </div>
-        {/* Entravaganza_Schedule_box */}
-        <div className='Entravaganza_Schedule_Box'>
-            <div className='Entravaganza_Schedule_heading'>Events Schedule</div>
-            {/* Schedule 17 April 17th, 2024 */}
-            <div>
-                <span className='Event_circle'></span>
-                <span className='Event_line'></span>
-                <div>
-                    <h3>INAUGURATION & B PLAN</h3>
+    return (
+        <div className='Entravaganza_Main'>
+            <div className='ParticleBg'>
+                <ParticleBg id="particles" className="particles" />
+            </div>
+            <div className='Entravaganza_Heading'>
+                Entravaganza
+            </div>
+            {/* Entravaganza_Schedule_box */}
+            <div className='Entravaganza_Schedule_Box'>
+                <div className='Entravaganza_Schedule_heading'>Events Schedule</div>
+                {/* Schedule 17 April 17th, 2024 */}
+                <div className='Events_box'>
+                    <div className='Event_flowline'>
+                        <span className='Event_circle'></span>
+                        <span className='Event_line'></span>
+                    </div>
+                    <div className=''>
+                        <h3>April 17th, 202</h3>
+                        <div className=''>
+                            <span>INAUGURATION</span>
+                            <span>South Campus</span>
+                        </div>
+                        <div>
+                            <span>Flash Mob</span>
+                            <span>North Campus</span>
+                        </div>
+                        <div>
+                            <span>B-Plan</span>
+                            <span>North Campuc</span>
+                            
+                        </div>
+                        <div></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Entravaganza
