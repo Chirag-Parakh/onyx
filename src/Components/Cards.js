b116ef1a7d42b34441ed6a6db1177f255c15f5be
Chirag Parakh <cparakh53@gmail.com>
2024-04-10 19:19:12 +0530

    final

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -9,54 +9,19 @@ function Cards() {
     <div className='cards'>
       <h1 className='Check_heading'>Check out our Events!</h1>
       <div className='cards__container'>
-        {/* <ul className='cards__items'> */}
-
         <CardItem
           src='images/Onyx3.jpeg'
           text='Explore the hidden waterfall deep inside the Amazon Jungle'
           label='Entravaganza'
           path='/Entravaganza'
         />
-
         <CardItem
           src='images/Onyx2.jpeg'
           text='Travel through the Islands of Bali in a Private Cruise'
           label='Inception'
           path='/event'
         />
-        {/* </ul> */}
-        {/* <ul className='cards__items'>
-            <CardItem
-              src='images/img.jpg'
-              text='Captivate was a 
-contest to bring out the inner entrepreneur.
-The event focused on the expression of thoughts
-and ideas using photography. '
-              label='Captivate'
-              path='/event'
-            />
-            <CardItem
-              src='images/img.jpg'
-              text='A mind-boggling event that stimulated the real things that go down when you pitch your business ideas, to sharks 
-only willing to empty their pockets for the most 
-impressive pitches. 
-'
-              label='Shark Tank'
-              path='/event'
-            />
-            <CardItem
-              src='images/img.jpg'
-              text='An event that 
-challenged problem-solving skills to explore 
-and find clues to 
-make it out first.
-'
-              label='Workshops'
-              path='/event'
-            />
-          </ul> */}
       </div>
-      {/* </div> */}
     </div>
   );
 }
