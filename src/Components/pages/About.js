2a36fa997023349d3fd8ed6719debfac8384c0de
Chirag Parakh <cparakh53@gmail.com>
2024-04-11 11:41:06 +0530

    final

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../App.css'
+import { GiTrophyCup } from "react-icons/gi";
 
 function About() {
   return (
@@ -11,9 +12,19 @@ function About() {
           Entrepreneurship Cell of NIE founded in 2009. We are a group of highly enthusiastic people who aim to bring an entrepreneurial spirit among the students and gradually expand over time.
         </div>
       </div>
+      
       <span className='verticle_line '></span>
-      <div className='achivements'>
-
+      <div className='Achivements_box'>
+        <div className='Achivements_heading green'>
+          Achievements
+        </div>
+        <div className='Achivements'>
+          <div className='Achivement'><GiTrophyCup className='green trophy'/>2023 IIT Bombay Avenue’s Pitch Perfect Winners</div>
+          <div className='Achivement'><GiTrophyCup className='green trophy'/>2021 E-Cell IIT Bombay Eureka Zonal Finalists</div>
+          <div className='Achivement'><GiTrophyCup className='green trophy'/>2016 NEN E-Week Championship Award</div>
+          <div className='Achivement'><GiTrophyCup className='green trophy'/>2015 NEN E-Week Championship Award</div>
+          <div className='Achivement'><GiTrophyCup className='green trophy'/>2013 NEN E-Week Championship Award</div>
+        </div>
       </div>
     </div>
   );
